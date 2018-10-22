59b9d9d41d0ba16f21acce7f7e12763b1a257892
jinalshah999 <jinalshah999@gmail.com>
2018-10-23 00:19:35 +0530

    dragdrop demo Angular CDK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop,moveItemInArray,transferArrayItem } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  todo=[
+    'write an email to your manager',
+    'push your code to github',
+    'go for running',
+    'go to movie',
+    'buy milk',
+    'brush your teeth',
+    'check your email'
+  ];
+  doneList=['done'];
+  constructor(){
+  }
+
+  ngOnInit(){
+ }
+ drop(event:CdkDragDrop<string[]>){
+  if(event.previousContainer===event.container){
+    moveItemInArray(this.todo,event.previousIndex,event.currentIndex);
+  }
+  else{
+    transferArrayItem(event.previousContainer.data,
+      event.container.data,event.previousIndex,event.currentIndex);
+  }
+
+
+ }
+
 }
